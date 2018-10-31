9d6875132fad0fbc261ca9e6fd335b52bf60efa4
Benjamin Morales <benjamin@bwmorales.com>
2018-10-31 07:17:35 -0500

    Update userIteration.sh

--- userIteration.sh
+++ userIteration.sh
@@ -2,7 +2,7 @@
 # Variables and functions that need to be accesible in subshells must be exported (duh).
 # Enclose arguments in single-quotes to preserve bash internal variables, etc.
 # Example Usage:
-# userIteration 'remove "$HOME/Library/Containers/com.microsoft.Excel"'
+# userIteration 'rm -Rf "$HOME/Library/Containers/com.microsoft.Excel"'
 
 userIteration() {
     for ITERATED_USER in $(dscl . list /Users UniqueID | awk '$2 >= 500 {print $1}'); do
